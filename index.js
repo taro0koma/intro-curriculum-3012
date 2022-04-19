ba49229a02fd1cccf394516bf53f8c284f4950ff
YURI <taro0koma@gmail.com>
2022-04-19 16:28:29 +0900

    サーバ起動日時のログ記録を実装

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 'use strict';
 const http = require('http');
 const server = http
-  .createServer((req, res) => {
-    console.info(
-      `[${new Date()}] Requested by ${req.socket.remoteAddress}`
-    );
-    res.writeHead(200, {
-      'Content-Type': 'text/plain; charset=utf-8'
+    .createServer((req, res) => {
+        console.info(
+            `[${new Date()}] Requested by ${req.socket.remoteAddress}`
+        );
+        res.writeHead(200, {
+            'Content-Type': 'text/plain; charset=utf-8'
+        });
+        res.write(req.headers['user-agent']);
+        res.end();
+    })
+    .on('error', e => {
+        console.error(`[${new Date()}] Server Error`, e);
+    })
+    .on('clientError', e => {
+        console.error(`[${new Date()}] Client Error`, e);
     });
-    res.write(req.headers['user-agent']);
-    res.end();
-  })
-  .on('error', e => {
-    console.error(`[${new Date()}] Server Error`, e);
-  })
-  .on('clientError', e => {
-    console.error(`[${new Date()}] Client Error`, e);
-  });
 const port = 8000;
 server.listen(port, () => {
-  console.log(`Listening on ${port}`);
+    // サーバをいつ立ち上げたかログを残したい console.log() => console.info()
+    // console.log(`Listening on ${port}`);
+    console.info(`[${new Date()}] Listening on ${port}`);
 });
